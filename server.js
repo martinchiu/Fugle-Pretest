9515b024e60081a46b5d7592924e8bfefee07382
KNOWHOW\martinchiu <martinchiu@knowhow.idv.tw>
2023-10-25 10:11:24 +0800

    feat: fulfill part two rate limiting

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 const express = require('express')
 const bent = require('bent')
+const { createClient } = require('redis')
 
 const app = express()
 const getJSON = bent('json')
+const redisClient = createClient()
+redisClient.on('error', err => console.log('Redis redisClient Error', err))
+redisClient.connect()
 
 const PORT = 5001
 const url = 'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty'
+const IPKEY = 'IP:'
+const USERKEY = 'USER:'
 
-app.get('/data', async (req, res) => {
+app.get('/data', rateLimiting, async (req, res) => {
     try {
         const data = await getJSON(url)
         const userId = Number(req.query.user)
-        const results = data.filter(ele => ele % userId === 0)
-        res.send(JSON.stringify({results}));
+        const result = data.filter(ele => ele % userId === 0)
+        res.send({result});
     } catch (error) {
         console.error(error)
         res.statusCode = 500
@@ -20,5 +26,24 @@ app.get('/data', async (req, res) => {
     }
 })
 
+async function rateLimiting (req, res, next) {
+    const ip = req.headers['x-real-ip'] || req.headers['x-forwarded-for'] || req.socket.remoteAddress || ''
+    const userId = Number(req.query.user)
+    const ipKey = `${IPKEY}${ip}`
+    const userKey = `${USERKEY}${userId}`
+    const ipCount = await redisClient.get(ipKey)
+    const idCount = await redisClient.get(userKey)
+    if (ipCount > 10 || idCount > 5) {
+        res.statusCode = 429
+        res.send({ip: ipCount, id: idCount});
+        next(new Error('exceed limit times'))
+    } else {
+        await redisClient.expire(ipKey, 60)
+        await redisClient.expire(userKey, 60)
+    }
+    await redisClient.incr(ipKey)
+    await redisClient.incr(userKey)
+    next()
+}
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
