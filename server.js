375eb2e64bdd7f1ec0a51b14198f0eff75cff048
KNOWHOW\martinchiu <martinchiu@knowhow.idv.tw>
2023-10-26 17:04:23 +0800

    feat: fulfill part three websocket api

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 const express = require('express')
 const bent = require('bent')
 const { createClient } = require('redis')
+const WebSocket = require('ws');
+const dayjs = require('dayjs')
 
 const app = express()
 const getJSON = bent('json')
+const SocketServer = WebSocket.Server
 const redisClient = createClient()
 redisClient.on('error', err => console.log('Redis redisClient Error', err))
 redisClient.connect()
 
 const PORT = 5001
 const url = 'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty'
+const wsUrl = 'wss://ws.bitstamp.net'
 const IPKEY = 'IP:'
 const USERKEY = 'USER:'
+const BITSKEY = 'BITS:'
+const currencyPairArr = ['btcusd', 'btceur', 'btcgbp', 'btcpax', 'gbpusd', 'eurusd', 'xrpusd', 'xrpeur', 'ltcbtc', 'ltcusd']
+let subscriptionList = []
 
 app.get('/data', rateLimiting, async (req, res) => {
     try {
@@ -46,4 +53,67 @@ async function rateLimiting (req, res, next) {
     next()
 }
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}`))
+async function setRedis (currencyPair, liveTicker, timestamp) {
+    const time = dayjs.unix(timestamp).format('YYYY-MM-DD HH:mm')
+    const redisData = await redisClient.HGET(`${BITSKEY}${currencyPair}`, time)
+    let dataObj
+    if (redisData) {
+        dataObj = JSON.parse(redisData)
+        dataObj.low = dataObj.low > liveTicker ? liveTicker : dataObj.low
+        dataObj.high = dataObj.high < liveTicker ? liveTicker : dataObj.high
+        dataObj.close = dataObj.liveTicker = liveTicker
+        await redisClient.HSET(`${BITSKEY}${currencyPair}`, time, JSON.stringify(dataObj))
+    } else {
+        dataObj = {}
+        dataObj.open = dataObj.high = dataObj.low = dataObj.close = dataObj.liveTicker = liveTicker
+        await redisClient.HSET(`${BITSKEY}${currencyPair}`, time, JSON.stringify(dataObj))
+        await redisClient.EXPIRE(`${BITSKEY}${currencyPair}`, 900)
+    }
+    return dataObj 
+}
+
+const server = app.listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+// get bitstamp data
+let wsClient = new WebSocket(wsUrl)
+wsClient.onopen = () => {
+    const WebSocketParam = {
+        event: 'bts:subscribe',
+        data: {}
+    }
+    currencyPairArr.forEach(ele => {
+        WebSocketParam.data.channel = `live_trades_${ele}`
+        wsClient.send(JSON.stringify(WebSocketParam))
+    })
+}
+wsClient.onmessage = async event => {
+    const result = JSON.parse(event.data)
+    const currencyPair = result.channel.replace('live_trades_', '')
+    if (Object.keys(result.data).length !== 0) {
+        const redisData = await setRedis(currencyPair, result.data.price, result.data.timestamp)
+        const responseObj = {}
+        responseObj[currencyPair] = redisData
+        subscriptionList.forEach((client) => {
+            client.send(JSON.stringify(responseObj))
+        })
+    }
+}
+
+// WebSocket server
+const wss = new SocketServer({ server })
+wss.on('connection', ws => {
+    console.log('Client connected')
+    ws.on('message', async data => {
+        data = JSON.parse(data.toString() ) 
+        if (data.action === 'subscribe') {
+            subscriptionList.push(ws)
+        } else if (data.action === 'unsubscribe') {
+            const index = subscriptionList.indexOf(ws)
+            if (index !== -1) subscriptionList.splice(index, 1)
+        }
+    })
+
+    ws.on('close', () => {
+        console.log('Close connected')
+    })
+})
